582078db1a0dd9adc568b315284532eecd54ecb0
YoussefKhaled03 <122903999+YoussefKhaled03@users.noreply.github.com>
2023-08-11 23:41:36 +0200

    try code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n ;
+    if (n == 0 ) printf("is zero");
+    else if ( n < 0 ) printf("is negative");
+    else printf("is positive");
+
+	return (0);
+}
