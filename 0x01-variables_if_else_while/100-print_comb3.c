f637c5fd4e068d6882bf430770cc0a2959d43aec
YoussefKhaled03 <122903999+YoussefKhaled03@users.noreply.github.com>
2023-08-12 01:44:31 +0200

    ff

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,8 @@ int main(void)
 			{
 				putchar(n);
 				putchar(m);
-				if (n == 56 && m == 57) break;
+				if (n == 56 && m == 57) 
+                    break;
                 putchar(',');
 				putchar(' ');
 			}
