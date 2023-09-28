21530d47900f0e0701d6a48994e22deee4c4c040
GOC <goc@localhost.localdomain>
2023-09-28 11:13:45 -0400

    nope

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,12 +2,13 @@
 #include <stddef.h>
 /**
  * get_bit - the value of a bit at a given index
- * @n: the number 
+ * @n: the number
  * @index: the index of the digit
  * Return: the index.
 */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if(index >63) return (-1);
 	return ((n >> index) & 1);
 }
