7175e72cb40080009b294ebbd9381b0ddad9ca54
GOC <goc@localhost.localdomain>
2023-09-28 11:29:48 -0400

    nope

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - the value of a bit at a given index
+ * @n: the number
+ * @index: the index of the digit
+ * Return: the index.
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > 63)
+		return (-1);
+	return (*n || (1 << index));
+}
